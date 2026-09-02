a43f13709812d597e345d279a3d8754a3b43f509
Samuel Raju Bethala <samuel@iitb.ac.in>
2026-09-02 17:14:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// IMPORTANT for GitHub Pages:
-// If you deploy to https://<user>.github.io/<repo-name>/, set `base` to
-// '/<repo-name>/' below (e.g. '/tp/'). If you deploy to a user/org page
-// (https://<user>.github.io/) or a custom domain, set base to '/'.
-const REPO_NAME = 'blck-iris.github.io';
-
 export default defineConfig({
-  plugins: [react()],
-  base: `/${REPO_NAME}/`,
+  plugins: [react()],
+  base: '/',
 });
-
-3
